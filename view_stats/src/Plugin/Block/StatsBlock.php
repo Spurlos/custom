8043c80e6345a91709ec09edb1ee6b96578e6727
Vladislav Shumkin <spurlos2000@gmail.com>
2016-06-11 01:00:58 +0300

    Add render array output and database queries

--- view_stats/src/Plugin/Block/StatsBlock.php
+++ view_stats/src/Plugin/Block/StatsBlock.php
@@ -22,13 +22,13 @@ class StatsBlock extends BlockBase {
    * {@inheritdoc}
    */
   public function build() {
-    $build = [];
-    $build['stats_block']['#markup'] = 'Implement StatsBlock!!.';
-
 
     $node = \Drupal::routeMatch()->getParameter('node');
     if ($node) {
       $nid = $node->id();
+    }
+    else {
+      return;
     }
     //dpm('nid '.$nid);
     $user = \Drupal::currentUser();
@@ -42,18 +42,17 @@ class StatsBlock extends BlockBase {
       ))
       ->execute();
     //dpm($query);
-    dpm('nid '.$nid);
+    //dpm('nid '.$nid);
     $query = db_select('node_stats')
       ->condition('nid', $nid, '=')
       ->fields('node_stats')
       ->countQuery()
       ->execute();
     $total_views = $query->fetchField();
-    dpm('Total views: '.$total_views);
+    //dpm('Total views: '.$total_views);
 
-    $start_time = strtotime('today');
-    dpm($start_time);
-    //$start_time = 1465563401;
+    //$start_time = strtotime('today');
+    $start_time = REQUEST_TIME-86400;
     $end_time = REQUEST_TIME;
     $query = db_select('node_stats')
       ->condition('nid', $nid, '=')
@@ -62,9 +61,46 @@ class StatsBlock extends BlockBase {
       ->countQuery()
       ->execute();
     $today_views = $query->fetchField();
-    dpm('Today views: '.$today_views);
+    //dpm('Today views: '.$today_views);
 
-    return $build;
+    $uid = db_select('node_stats')
+      ->orderBy('timestamp', 'DESC')
+      ->range(0,1)
+      ->fields('node_stats', array('uid'))
+      ->execute()
+      ->fetchField();
+
+    $user = user_load($uid);
+    $username = $user -> getUsername();
+    //dpm('Last viewed by '.$username.' at '.date("Y-m-d H:i:s ", REQUEST_TIME));
+
+
+    /*$user_query = db_select('users_field_data')
+      ->fields('users_field_data', array('uid','name'));
+
+    $last_username = $query->join($user_query, 'lastuser', 'node_stats.uid = myalias.uid');
+    dpm($last_username);*/
+
+    $date = format_date(REQUEST_TIME);
+    //dpm($date);
+    $output = array(
+      'first_p' => array(
+        '#type' => 'markup',
+        '#markup' => $this->t('Number of views: :today today / :total total',
+          [':today' => $today_views,
+          ':total' => $total_views]
+        ),
+      ),
+      'second_p' => array(
+        '#type' => 'markup',
+        '#markup' => $this->t('<p>Last viewed by: :user at :date',
+          [':user' => $username,
+          ':date' => $date]
+        )
+      ),
+
+    );
+    return $output;
   }
 
 }
